a7b7aab537f7e4d993fa847ec58b02d9b62f5713
pascal struck <struck_pascal@web.de>
2020-12-10 15:23:00 +0100

    bugfix

--- lineclock/sketch.js
+++ lineclock/sketch.js
@@ -10,26 +10,23 @@ function setup() {
 function draw() {
   background(220);
 
-
-
-
   push()
   drawingContext.shadowBlur = 3;
   drawingContext.shadowColor = color(0);
   noStroke();
-  //fill(255,0,0);
+
   for (let i=0; i<second();i++){
   	fill(220);
   	ellipse(20+i*23,300,10,10);
   }
 
-  //fill(0,255,0);
   for (let i=0; i<minute();i++){
+    fill(220);
   	ellipse(20+i*23,400,10,10);
   }
 
-  //fill(0,0,255);
   for (let i=0; i<hour();i++){
+    fill(220);
   	ellipse(20+i*60,500,10,10);
   }
   pop();
