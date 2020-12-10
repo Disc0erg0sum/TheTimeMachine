82358530ae309cb323e3d0fe8081c7703cc60c09
Jendrik Bradaczek <jendrikbradaczek@Jendriks-MacBook-Pro.local>
2020-12-10 02:06:02 +0100

    init

---
+++ clock1/sketch.js
@@ -0,0 +1,49 @@
+let s, m ,h;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  rectMode(CENTER);
+  draw_sec(second());
+  draw_min(minute());
+  draw_hour(hour());
+
+}
+
+
+function draw_sec(s){
+  push();
+  translate(width/2 - 70, height/2);
+  fill(0,0,0,0);
+  stroke(1);
+  square(0, 0, 60);
+  fill(10);
+  square(0,0,s);
+  pop()
+}
+
+function draw_min(m) {
+  push();
+  translate(width/2, height/2);
+  fill(0,0,0,0);
+  stroke(1);
+  square(0, 0, 60);
+  fill(10);
+  square(0,0,m);
+  pop()
+}
+
+function draw_hour(h) {
+  push();
+  translate(width / 2 + 70, height / 2);
+  fill(0, 0, 0, 0);
+  stroke(1);
+  square(0, 0, 60);
+  fill(10);
+  square(0, 0, mag(h, 0, 23, 0, 59));
+  pop();
+}
+
