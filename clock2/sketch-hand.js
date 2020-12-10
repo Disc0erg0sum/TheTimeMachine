80573633d2a43379de1a4df7aa34bbb42dd88ba0
Disc0erg0sum <jendrik@freezeframes.de>
2020-12-10 02:59:19 +0100

    edit

--- clock2/sketch-hand.js
+++ clock2/sketch-hand.js
@@ -1,68 +1,64 @@
-const canvas1 = function (p) {
-  sec = null;
-  min = null;
-  hour = null;
+sec = null;
+min = null;
+hour = null;
 
-  p.setup = function () {
-    p.createCanvas(800, 800);
-  };
-
-  p.draw = function () {
-    p.background(220);
-    p.makeClockBase();
-    p.showHouerHand();
-    p.showMinHand(100);
-    p.showSecHand(80);
-    p.updateTime();
-  };
-
-  p.updateTime = function () {
-    sec = (sec + 1) % 60;
-    // console.log("sec:" + sec);
-
-    if (sec == 0) {
-      min = (min + 1) % 60;
-      console.log("min" + min);
-    }
-
-    if (sec == 0 && min == 0) {
-      hour = (hour + 1) % 12;
-      console.log("houer" + hour);
-    }
-  };
-
-  p.makeClockBase = function () {
-    p.translate(p.width / 2, p.height / 2);
-    p.ellipseMode(p.CENTER);
-    p.ellipse(0, 0, 500, 500);
-  };
-
-  p.showSecHand = function (offset) {
-    p.translate(offset, 0);
-    angel = p.map(sec, 0, 60, 0, 360);
-    p.rotate(p.radians(angel));
-    p.stroke(100, 0, 0);
-    p.strokeWeight(2);
-    p.line(0, 0, 60, 0);
-  };
-
-  p.showMinHand = function (offset) {
-    p.translate(offset, 0);
-    angel = p.map(min, 0, 60, 0, 360);
-    p.rotate(p.radians(angel));
-    p.stroke(10, 100, 10);
-    p.strokeWeight(4);
-    p.line(0, 0, 80, 0);
-  };
-
-  p.showHouerHand = function () {
-    p.rotate(p.radians(-90));
-    angel = p.map(hour % 12, 0, 12, 0, 360);
-    p.rotate(p.radians(angel));
-    p.stroke(10);
-    p.strokeWeight(6);
-    p.line(0, 0, 100, 0);
-  };
+setup = function () {
+  createCanvas(800, 800);
 };
 
-var myp5 = new p5(canvas1, "canvas1");
+draw = function () {
+  background(220);
+  makeClockBase();
+  showHouerHand();
+  showMinHand(100);
+  showSecHand(80);
+  updateTime();
+};
+
+updateTime = function () {
+  sec = (sec + 1) % 60;
+  // console.log("sec:" + sec);
+
+  if (sec == 0) {
+    min = (min + 1) % 60;
+    console.log("min" + min);
+  }
+
+  if (sec == 0 && min == 0) {
+    hour = (hour + 1) % 12;
+    console.log("houer" + hour);
+  }
+};
+
+makeClockBase = function () {
+  translate(width / 2, height / 2);
+  ellipseMode(CENTER);
+  ellipse(0, 0, 500, 500);
+};
+
+showSecHand = function (offset) {
+  translate(offset, 0);
+  angel = map(sec, 0, 60, 0, 360);
+  rotate(radians(angel));
+  stroke(100, 0, 0);
+  strokeWeight(2);
+  line(0, 0, 60, 0);
+};
+
+showMinHand = function (offset) {
+  translate(offset, 0);
+  angel = map(min, 0, 60, 0, 360);
+  rotate(radians(angel));
+  stroke(10, 100, 10);
+  strokeWeight(4);
+  line(0, 0, 80, 0);
+};
+
+showHouerHand = function () {
+  rotate(radians(-90));
+  angel = map(hour % 12, 0, 12, 0, 360);
+  rotate(radians(angel));
+  stroke(10);
+  strokeWeight(6);
+  line(0, 0, 100, 0);
+};
