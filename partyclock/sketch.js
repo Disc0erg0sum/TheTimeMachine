f4892185952efd3a57cbeb1715a29ff212b6f612
pascal struck <struck_pascal@web.de>
2020-12-17 21:14:08 +0100

    Update sketch.js

--- partyclock/sketch.js
+++ partyclock/sketch.js
@@ -1,29 +1,45 @@
-var num = 30;
+var numw;
+var numh;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(20);
+  background(224, 198, 92);
+
+  //numw = windowWidth/80;
+  //numh = windowHeight/80;
+
+  numw = 32;
+  numh = 32;
+
+
 }
 
 function draw() {
-  background(20);
+  background("#DAD8A7");
 
-  for (let i = 0; i < num; i++) {
-    stroke(0,255,0);
-    strokeWeight(4);
-    line(0+hour()*60,0,0+i*60,height);
+  let s = map(second(),0,60,0,height);
+  let m = map(minute(),0,60,0,height);
+  let h = map(hour(),0,24,0,width);
+
+
+  for (let i = 0; i < numw; i++) {
+    stroke("#99B898");
+    strokeWeight(min(width,height)/90);
+    line(h,0,i*width/numw,height);
   }
 
-  for (let i = 0; i < num; i++) {
-    stroke(0,0,255);
-    strokeWeight(4);
-    line(0,0+minute()*10,width,0+i*26);
+
+  for (let i = 0; i < numh; i++) {
+    stroke("#E84A5F");
+    strokeWeight(min(width,height)/200);
+    line(0,m,width,i*height/numh);
   }
 
-  for (let i = 0; i < num; i++) {
-    stroke(255,0,0);
-    strokeWeight(4);
-    line(0,0+i*26, width,0+second()*10);
+
+  for (let i = 0; i < numh; i++) {
+    stroke("#2A363B");
+    strokeWeight(min(width,height)/300);
+    line(0,i*height/numh,width,s);
   }
 
 }
