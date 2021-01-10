0feaaca01795b1d8985cac93a933acb7d648bff2
pascal struck <struck_pascal@web.de>
2021-01-10 01:12:09 +0100

    color, resize

--- barcodeclock/sketch.js
+++ barcodeclock/sketch.js
@@ -1,12 +1,18 @@
+var canvas;
 
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(10);
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0,0);
+  canvas.style('z-index','-1');
+  background(235);
 }
 
 function draw() {
-  background(10);
+  background(235);
 
   let mil = map(millis()%1000,0,1000,0,windowWidth);
   let sec = map(second(),0,60,0,windowWidth);
@@ -16,7 +22,7 @@ function draw() {
   //text(millis(),200,200);
 
 
-  stroke(255);
+  stroke(10);
   strokeWeight(5);
 
   line(h,0,h,height);
