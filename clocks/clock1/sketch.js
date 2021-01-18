bf5efeb649504c213bf38264919c74bb57b32e89
Disc0erg0sum <jenrik@freezeframes.de>
2021-01-18 22:48:45 +0100

    + pattern

--- clocks/clock1/sketch.js
+++ clocks/clock1/sketch.js
@@ -1,4 +1,6 @@
-let s, m, h;
+let time = [];
+let time_i = [];
+let pat = [];
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
@@ -8,51 +10,60 @@ function setup() {
   pixelDensity(displayDensity());
   createCanvas(windowWidth, windowHeight);
   smooth();
+  frameRate(2);
+
+  init_pattern(25, 40);
 }
 
 function draw() {
   background("#f9faff");
-  rectMode(CENTER);
-  draw_sec(second());
-  draw_min(minute());
-  draw_hour(hour());
+  nrom_time();
+  for (let i = 0; i < pat.length; i++) {
+    for (let j = 0; j < pat[i].length; j++) {
+      pat[i][j].update(time[time_i[i][j]]);
+      pat[i][j].show();
+    }
+  }
 }
 
-function draw_sec(s) {
-  push();
-  translate(width / 2 - 70, height / 2);
-  fill(0, 0, 0, 0);
-  stroke(1);
-  square(0, 0, 60);
-  fill(10);
-  square(0, 0, s);
-  pop();
+function init_pattern(x, y) {
+  let d = 50;
+  for (let i = 0; i < x; i++) {
+    time_j = [0, 1, 2];
+    pat[i] = [];
+    time_i[i] = [];
+    for (let j = 0; j < y; j++) {
+      pat[i][j] = new squr(i * d, j * d, d, color(50));
+      time_i[i][j] = random(time_j);
+    }
+  }
 }
 
-function draw_min(m) {
-  push();
-  translate(width / 2, height / 2);
-  fill(0, 0, 0, 0);
-  stroke(1);
-  square(0, 0, 60);
-  fill(10);
-  square(0, 0, m);
-  pop();
+function nrom_time() {
+  time[0] = second() / 59;
+  time[1] = minute() / 59;
+  time[2] = hour() / 23;
 }
 
-function draw_hour(h) {
-  push();
-  translate(width / 2 + 70, height / 2);
-  fill(0, 0, 0, 0);
-  stroke(1);
-  square(0, 0, 60);
-  fill(10);
-  square(0, 0, mag(h, 0, 23, 0, 59));
-  pop();
-}
-
-class rec{
-  constructor(x,y,w,time){
+class squr {
+  constructor(x, y, d, c) {
     this.x = x;
+    this.y = y;
+    this.d = d;
+    this.c = c;
+    this.t;
+  }
+
+  update(t) {
+    this.t = t;
+  }
+
+  show() {
+    push();
+    translate(this.x, this.y);
+    fill(this.c);
+    strokeWeight(0);
+    square(0, 0, this.d * this.t);
+    pop();
   }
 }
